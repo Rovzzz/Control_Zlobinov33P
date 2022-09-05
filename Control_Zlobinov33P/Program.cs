a6a0164d797fcdac32d3235b548cc44064e00fcf
unknown <@ngknn.local>
2022-09-05 18:15:41 +0300

    First Commit

---
+++ Control_Zlobinov33P/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Zlobinov33P
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Номер 1
+            //Написать программу, которая запрашивает ввод с клавиатуры двух положительных вещественных чисел и вычисляет логарифм первого числа по основанию второго.
+
+            //for (; ; )
+            //{
+            //    Console.Write("\nВведите x: ");
+            //    double x = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("\nВведите y: ");
+            //    double y = Convert.ToDouble(Console.ReadLine());
+            //    double otvet;
+            //    if (x < 0 || y < 0)
+            //    {
+            //        Console.WriteLine("\nВведите положительные числа!");
+            //    }
+            //    else
+            //    {
+            //        otvet = Math.Log(x, y);
+            //        Console.Write($"\nОтвет: {otvet}");
+            //    }
+            //}
+            #endregion
+            #region Номер 3
+            // Напишите программу, которая проверяет, дает ли введенное с клавиатуры число одинаковый остаток при делении на 3 и на 4.
+
+            //Console.Write("\nВведите x: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int ostatok1 = x % 3;
+            //Console.WriteLine($"{x} / 3 = {ostatok1}");
+            //int ostatok2 = x % 4;
+            //Console.WriteLine($"{x} / 4 = {ostatok2}");
+            //if (ostatok1 == ostatok2)
+            //{
+            //    Console.WriteLine("Одинаковый остаток");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Разный остаток");
+            //}
+            #endregion
+            #region Номер 5
+            //Console.Write("\nВведите x: ");
+            //double x = Convert.ToInt32(Console.ReadLine());
+            //double otvet = Math.Log(Math.Abs(Math.Cos(x))) / Math.Log(1 + Math.Pow(x, 2));
+            //Console.WriteLine(otvet);
+            #endregion
+            #region Номер 8
+            //Пользователь вводит три действительных (вещественных) числа. Нужно определить, является ли сумма каких-либо двух из этих чисел отрицательной
+            //Console.Write("Введите x: ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("\nВведите y: ");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("\nВведите z: ");
+            //double z = Convert.ToDouble(Console.ReadLine());
+            //double a = x + y;
+            //Console.WriteLine($"\n{x} + {y} = {a}");
+            //double b = y + z;
+            //Console.WriteLine($"\n{y} + {z} = {b}");
+            //double c = x + z;
+            //Console.WriteLine($"\n{x} + {z} = {c}");
+            //if (a < 0 || b < 0 || c < 0)
+            //{
+            //    Console.WriteLine("\nСумма чисел меньше 0");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\nСумма чисел больше 0");
+            //}
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
